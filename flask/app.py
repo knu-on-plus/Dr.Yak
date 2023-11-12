244afcda822b7f995125f028269f3f623dedafe9
Hojun Song <hojunking3@gmail.com>
2023-11-12 17:54:34 +0900

    Refactoring

--- flask/app.py
+++ flask/app.py
@@ -1,17 +1,9 @@
-from flask import Flask, request, render_template, send_from_directory, url_for, redirect
-# chatGPT 라이브러리가 실제로 존재하지 않으므로 대체되었습니다. 적절한 라이브러리를 사용해주세요.
-# import chatGPT
-import tempfile
-import os
-import torch
-import time
-import TTS
-from ultralytics import YOLO
+from flask import Flask, request, render_template, url_for, redirect
+import os, uuid
+import preprocessing, postprocessing, sentimental, ocr, yolo_classifier, database, ensemble
+#import TTS
 
-dic = {0: ['레피콜', '감기약', '1일 3회 식후 30분에 2캡슐씩 복용 가능합니다.'], 
-       1: ['타이레놀', '진통제', '1회 1~2정씩 1일 3~4회 필요시 복용합니다. 1일 최대 8정 복용 가능합니다.'],
-       2: ['제로민', '수면제', '1일 1회 취침 전 복용합니다.']
-        }
+
 
 app = Flask(__name__)
 
@@ -25,35 +17,36 @@ def form():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-
-    ##이미지, 오디오 초기화
-    # if os.path.exists('static/uploads/uploaded_image.png'):
-    #     os.remove('static/uploads/uploaded_image.png')
-    # if os.path.exists('static/temp_description.mp3'):
-    #     os.remove('static/temp_description.mp3')
-    #     print("audio deleted")
-
+    uploaded_uuid = str(uuid.uuid4())
+    uploaded_file_path = f'static/uploads/{uploaded_uuid}.png'
+    
+    # 업로드 파일 저장
     uploaded_file = request.files['image']
-    print(uploaded_file)
-    uploaded_file.save('static/uploads/uploaded_image.png')
-    model = YOLO("yolo_clf.pt")
-    results = model('static/uploads/uploaded_image.png') 
-    result_name = dic[results[0].probs.top1][0]
-    result_type = dic[results[0].probs.top1][1]
-    result_desc = f"{result_type}, {result_name}의 복용방법과 주의사항을 알려드릴게요." +"   "+ dic[results[0].probs.top1][2]
-    print(result_name)
-    print(result_type)
-    print(result_desc)
-    filename = TTS.save_tts(result_desc)
-    return render_template("result.html", result_name = result_name, result_type = result_type, filename = filename)
+    uploaded_file.save(uploaded_file_path)
 
+    # 이미지 전처리
+    preprocessed_path =  f'static/preprocessed_img'
+    img_path = preprocessing.transform_img(uploaded_file_path, preprocessed_path, uploaded_uuid)
+    
+    # OCR 모델 결과
+    ocr_result = ocr.paddle_ocr(img_path)
 
-# @app.route('/result', methods = ['GET', 'POST'])
-# def result():
-#     result_name = request.args.get('result_name')
-#     result_type = request.args.get('result_type')
-#     print(result_type, result_name)
-#     return render_template("result.html", result_name = result_name, result_type = result_type)
+    # 약 이미지 분류기
+    classification_result =yolo_classifier.classifier(img_path)
+
+    # OCR 모델에 대한 sentimental 모델 결과
+    sentimental_result = sentimental(ocr_result)
+    
+    # OCR 모델에 대한 sentimental 모델 결과와 약 이미지 분류기 앙상블 (voting) 결과 idx 
+    idx = ensemble(sentimental_result, classification_result)
+    
+    # Text To Speech 음성 파일 실행
+    #tts_filename = TTS.save_tts(idx)
+
+    # Database에서 해당 약에 대한 정보 불러오기
+    result_name, result_type = database.result(idx)
+
+    return render_template("result.html", result_name = result_name, result_type = result_type, filename = tts_filename)
     
 @app.route('/goback', methods=['GET'])
 def go_back():
---
+++ flask/ensemble.py
@@ -0,0 +1,5 @@
+
+
+def make_decision(sentimental_result, classification_result):
+    idx = ''
+    return idx
