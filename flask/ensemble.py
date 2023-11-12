0f9a12479ac88b1583e6049cd6b1928909095739
Hojun Song <hojunking3@gmail.com>
2023-11-12 20:50:17 +0900

    Add ensemble method

--- flask/ensemble.py
+++ flask/ensemble.py
@@ -1,5 +1,10 @@
 
 
-def make_decision(sentimental_result, classification_result):
-    idx = ''
-    return idx
+def soft_voting(sentimental_result, classification_result, weight1=0.7, weight2=0.3):
+    final_pred = []
+    for p1, p2 in zip(sentimental_result, classification_result):
+        # Calculate the weighted average probabilities across all classes
+        avg_prob = [(x * weight1 + y * weight2) for x, y in zip(p1, p2)]
+        # Choose the class with the highest weighted average probability
+        final_pred.append(avg_prob.index(max(avg_prob)))
+    return final_pred[0]
